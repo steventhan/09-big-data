e801e2d7f6762c102bbfdce3f964cd539f564f19
Code Fellows <codefellows@Stevens-MacBook-Pro.local>
2016-06-16 14:00:19 -0700

    switch driver

--- scripts/zip.js
+++ scripts/zip.js
@@ -5,11 +5,24 @@
     }
   };
 
-  
+  Zip.allZip = [];
 
   Zip.fetchAll = function() {
     $.getJSON('/data/manhattan.json', function(data) {
-
+      Zip.allZip = data.features.map(function (zip) {
+        return zip.properties;
+      }).reduce(function (acc, cur, indx, array) {
+        cur.coordinates = data.features[indx].geometry.coordinates;
+        acc.push(cur)
+        return acc;
+      }, []).map(function (zip) {
+        return {
+          "zip" : zip.zip,
+          "address" : zip.address,
+          "neighborhood" : zip.neighborhood,
+          "coordinates" : zip.coordinates
+        }
+      });
     });
   };
 
